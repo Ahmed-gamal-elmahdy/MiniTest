768e4efed3f5f8009ed032850931c71f7742ecc5
Ahmed-gamal-elmahdy <xda2014@hotmail.com>
2020-04-13 10:20:31 +0200

    Update encryption.java

--- encryption.java
+++ encryption.java
@@ -7,32 +7,29 @@ public class encryption {
 		System.out.println("Enter the string :");
 		String inputString = input.next();
 		System.out.println("Enter Key ");
-		int key = input.nextInt();
-		String binaryKey = Integer.toBinaryString((1<<8)|key).substring(1);
-		String finalWord = "";
-		finalWord = encryptThis(inputString, binaryKey);
+		int key = keyRange(input.nextInt());
+		String finalWord = encryptThis(inputString, key);
 		System.out.print(finalWord);
-		
 
+	}
 
-}
-
-
-	public static String encryptThis(String word, String key) {
+	public static String encryptThis(String word, int key) {
 		String output = "";
 		for (int i = 0; i < word.length(); i++) {
 			String charBin = charToBinary(word.charAt(i));
-			output += binaryToChar(xorKey(charBin, key));
+			//String nan = lengthEncFunction(charBin);
+			output += binaryToChar(xorKey(charBin, binaryKey(key + i).substring(1)));
 		}
 		return output;
 	}
 
-	public static void decryptThis(String word, int key) {
-
+	public static String binaryKey(int key) {
+		String output = Integer.toBinaryString((1 << 16) | key);
+		return output;
 	}
 
 	public static String charToBinary(char c) {
-		String number = Integer.toBinaryString( (1<<8)|(int) c).substring(1);
+		String number = Integer.toBinaryString((1 << 16) | (int) c).substring(1);
 		return number;
 	}
 
@@ -43,12 +40,30 @@ public class encryption {
 	}
 
 	public static String xorKey(String binaryNum, String key) {
-		
+
 		String output = "";
 		for (int i = 0; i < binaryNum.length(); i++) {
 			output += binaryNum.charAt(i) ^ key.charAt(i);
 		}
 		return output;
+	}
+
+	public static String lengthEncFunction(String inputWord) {
+		String output = "";
+		for (int i = 0; i < inputWord.length(); i++) {
+			output += inputWord.charAt(inputWord.length() - 1 - i);
+		}
+		return output;
+	}
+
+	public static int keyRange(int key) {
+		while (key > 97) {
+			key = key / 3;
+		}
+		while (key < 10) {
+			key *= 5;
+		}
+		return key;
 	}
 
 }
